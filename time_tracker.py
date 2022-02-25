4d5ea3e693563f078843026e52689f440cda6ab6
Filip Wróblewski <75478989+filipwroblewski@users.noreply.github.com>
2022-02-25 20:28:08 +0100

    Update time_tracker.py

--- time_tracker.py
+++ time_tracker.py
@@ -1,6 +1,7 @@
-import time
+import time, datetime
 import win32gui
 import json
+import os.path
 
 
 def time_convert(sec):
@@ -10,16 +11,31 @@ def time_convert(sec):
   mins = mins % 60
   return "{0}h {1}m {2}s".format(int(hours),int(mins),sec)
 
-def to_json_file(dictionary, f_name):
-	# Serializing json 
+def check_if_file_exist():
+	with open(get_file_name(), "r") as read_file:
+		data = json.load(read_file)
+
+	print(data)
+
+def get_file_name():
+	today = datetime.datetime.now()
+	f_name = f'{today.year}_{today.month}_{today.day}_{today.minute}.json'
+	return f_name
+
+def to_json_file(dictionary):
+	# file_exists = os.path.exists(f"{f_name}.json")
+	# print(f"file_exists = {file_exists}")
+	# Serializing json
 	json_object = json.dumps(dictionary, indent = 4)
-	with open(f"{f_name}.json", "w") as outfile:
+	with open(get_file_name(), "w") as outfile:
 		outfile.write(json_object)
 
 
 all_windows = {}
 active_window_name = ''
 stopperStatus = ''
+
+# check_if_file_exist()
 while True:
 	new_window_name = win32gui.GetWindowText(win32gui.GetForegroundWindow())
 	
@@ -36,7 +52,7 @@ while True:
 				all_windows[active_window_name] = round(time_lapsed)
 
 			print(active_window_name, all_windows[active_window_name])
-			to_json_file(all_windows, "all_windows")
+			to_json_file(all_windows)
 
 			
 		stopperStatus = 'start'
